f4b618224fb6a080213631c35df9621c007ec3a1
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-10-07 10:06:34 -0700

    feature(register and login): added register and login routes with controllers

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,8 @@ const {
   createRecipe,
   updateRecipe,
   searchByKeyword,
+  registerNewUser,
+  loginUser
 } = require("./controllers");
 
 router.get("*/recipes", getAllRecipes);
@@ -17,6 +19,8 @@ router.get("/recipes/search", searchByKeyword);
 router.get("/recipes/:id", getRecipeById);
 router.delete("/recipes/delete/:id", deleteRecipeById);
 router.post("/recipes/create", createRecipe);
+router.post("/users/register", registerNewUser);
+router.post("/users/login", loginUser);
 router.put("/recipes/update/:id", updateRecipe);
 
 module.exports = router;
