cf9e31dc64ef4c063090a6fde2b7f8c5b2ccf666
Matthew Jovenal <matthewjovenal1@outlook.com>
2023-01-27 12:21:57 -0700

    commit to pull on different device

---
+++ src/components/BookComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Book = () => {
+  return (
+  );
+};
+
+export default Book;
---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>SpongeBob Recipe Book</h1>
+      <h2>
+        Welcome to the SpongeBob recipe book! Here you will find all the
+        delicious recipes from the wonderful world of Bikin Bottom
+      </h2>
+    </div>
+  );
+};
+
+export default HomePage;
